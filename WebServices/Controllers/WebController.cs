1472a291d46690d05f0f417f4bc149e347425fff
Mario Andres Botero Florez <mariobot113@hotmail.com>
2018-10-18 16:57:18 -0500

    Add Service Project

---
+++ WebServices/Controllers/WebController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using WebServices.Models;
+
+namespace WebServices.Controllers
+{
+    public class WebController : ApiController
+    {
+        private ReservationRepository repo = new ReservationRepository();
+
+        public IEnumerable<Reservation> GetAllReservations() {
+            return repo.GetAll();
+        }
+
+        public Reservation GetReservation(int id) {
+            return repo.Get(id);
+        }
+
+        [HttpPost]
+        public Reservation CreateReservation(Reservation item){
+            return repo.Add(item);
+        }
+
+        [HttpPut]
+        public bool UpdateReservation(Reservation item)
+        {
+            return repo.Update(item);
+        }
+
+        public void DeleteReservation(int id) {
+            repo.Remove(id);
+        }
+    }
+}
